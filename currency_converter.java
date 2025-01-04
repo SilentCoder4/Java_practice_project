c3f57e9a72715dc4b0eaa829ca53ed2eead7ffde
Ankit Halder <halderankit9@gmail.com>
2025-01-04 23:14:08 +0530

    base currency converter structure

--- currency_converter.java
+++ currency_converter.java
@@ -1,5 +1,33 @@
+import java.io.*;
+import java.util.*;
 public class currency_converter {
     public static void main(String args[])  {
-        System.out.println("converter");
+
+        HashMap<Integer, String> curencycodes = new HashMap<Integer,String>();
+
+        curencycodes.put(1, "USD");
+        curencycodes.put(2, "INR");
+
+        String fromCode, toCode;
+        double amount;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to the currency converter: ");
+
+        System.out.println("Currency converting From? ");
+        System.out.println("1 : USD (US Dollar)\t 2 : INR (Indian Rupee)");
+        fromCode = curencycodes.get(sc.nextInt());
+
+        System.out.println("Currency converting To? ");
+        System.out.println("1 : USD (US Dollar)\t 2 : INR (Indian Rupee)");
+        fromCode = curencycodes.get(sc.nextInt());
+
+        System.out.println("Amount you wish to convert?");
+        amount = sc.nextFloat();
+
+//        sendHttpGETRequest(fromCode, toCode, amount);
+
+        System.out.println("Thank you for using the currency converter");
     }
 }
