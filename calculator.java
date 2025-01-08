ff2e42954d8f0ab5222856c53bc8a32375f8653b
Ankit Halder <halderankit9@gmail.com>
2025-01-08 23:47:01 +0530

    create button

--- calculator.java
+++ calculator.java
@@ -32,7 +32,32 @@ class calculator extends JFrame implements ActionListener {
 
          l.setEnabled(false);   //set textfield to non-editable
 
-         JButton b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, ba, bs, bd, bm, be, beq, beq1;
+         JButton b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, ba, bs, bd, bm, be, beq, beq1;     //create numbers and operators
+
+         //create number buttons
+         b0 = new JButton("0");
+         b1 = new JButton("1");
+         b2 = new JButton("2");
+         b3 = new JButton("3");
+         b4 = new JButton("4");
+         b5 = new JButton("5");
+         b6 = new JButton("6");
+         b7 = new JButton("7");
+         b8 = new JButton("8");
+         b9 = new JButton("9");
+         //create operation button
+         ba = new JButton("+");
+         bs = new JButton("-");
+         bd = new JButton("/");
+         bm = new JButton("*");
+         beq = new JButton("C");
+         // equals button
+         beq1 = new JButton("=");
+         // create . button
+         be = new JButton(".");
+
+         JPanel p = new JPanel();   //
+
 
      }
 
